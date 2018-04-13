86e581c751f1166e80299e2a69d6f0573eed75c1
JYeon0129 <wkddus119@naver.com>
2018-04-13 15:59:40 +0900

    Add files via upload
    하나의 상자에는 1개 또는 2개의 cowbell을 넣을수 있다.
    cowbell의 크기는 양의 정수이고 상자의 크기는 들어간 cowbell의 크기의 합과 같다
    최소한의 상자의 크기를 구하라

---
+++ 604B. More Cowbell.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+#include<algorithm>
+#include<functional> 
+#include<vector>
+#include<map>
+#include<conio.h>
+using namespace std;
+int *cowbell;
+int main() {
+	int n, k, index = 0, result = 0, single = 0;
+	cin >> n >> k;
+	cowbell = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> cowbell[i];
+	}
+	result = cowbell[n - 1];
+	if (k < n) {
+		single = 2 * k - n;
+		n -= single;
+		while (1) {
+			if (result < cowbell[index] + cowbell[n - 1 - index] && (n - 1 - index) >= 0) {
+				result = cowbell[index] + cowbell[n - 1 - index];
+			}
+			index++;
+			if (index > n - 1 - index) break;
+		}
+	}
+	cout << result;
+	//_getch();
+	return 0;
+}
