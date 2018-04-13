49cfb22e1741146f848d3902b0faaa2b11161f50
JYeon0129 <wkddus119@naver.com>
2018-04-13 14:36:08 +0900

    Add files via upload
    큰 상점 업타운이 있습니다. 말렉은 거기에서 그녀를 위해 고기를 사려고합니다. i 번째 날에는 1 킬로그램 당 pi 달러로 고기를 판매합니다. 말렉은 모든 수 a1, ..., an과 p1, ..., pn을 알고 있습니다. 매일 매일 고기를 임의로 살 수 있으며 장래에 가질 고기도 가질 수 있습니다.
    
    말렉은 고기 요리에서 조금 지쳐서 도움을 청했습니다. Duff가 n 일 동안 행복하게 지내기 위해 그가 소비하는 총비용을 최소화하도록 도와주세요.
    
    Input
    입력의 첫 번째 줄에는 정수 n (1 ≤ n ≤ 10^5), 일수가 포함됩니다.
    
    다음 n 줄에서 i 번째 줄에는 두 개의 정수 ai와 pi (1 ≤ ai, pi ≤ 100), 더프 고기 필요량 및 그 날의 고기 비용이 포함됩니다.
    
    Output
    Duff를 n 일 동안 행복하게 유지하는 데 필요한 최소 금액을 한 줄에 인쇄하십시오.

---
+++ 588A - Duff and Meat.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+#include<algorithm>
+#include<functional> 
+#include<vector>
+#include<map>
+#include<conio.h>
+using namespace std;
+int *arr_a;
+int *arr_p;
+int main() {
+	int n, result = 0, now_min = 101;
+	cin >> n;
+	arr_a = new int[n];
+	arr_p = new int[n];
+	for (int i = 0; i < n; i++) cin >> arr_a[i] >> arr_p[i];
+	for (int i = 0; i < n; i++) {
+		if (now_min > arr_p[i]) now_min = arr_p[i];
+		result += arr_a[i] * now_min;
+	}
+	cout << result;
+	//_getch();
+	return 0;
+}
