6e7ef3f9694284f4f95b5008be577207b83c1a18
JYeon0129 <wkddus119@naver.com>
2018-04-13 14:51:26 +0900

    Add files via upload
    각 체스 조각에 대해 우리는 체중을 알고 있습니다 :
    
    여왕의 무게는 9,
    루크의 체중은 5,
    주교의 무게는 3,
    기사의 무게는 3,
    폰의 무게는 1,
    왕의 체중은 위치를 평가할 때 고려되지 않습니다.
    플레이어의 체중은 보드에있는 모든 체중의 합계와 같습니다.
    
    A가 세는 것을 좋아하지 않으므로, 그는 더 큰 체중을 가진 선수를 결정할 수 있도록 도와달라고 요청했습니다.

---
+++ A. A and B and Chess.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+#include<algorithm>
+#include<functional> 
+#include<vector>
+#include<map>
+#include<conio.h>
+using namespace std;
+
+int main() {
+	char tmp; 
+	int result = 0;
+	for (int i = 0; i < 64; i++) {
+		cin >> tmp;
+		if (tmp == 'Q') result += 9;
+		if (tmp == 'R') result += 5;
+		if (tmp == 'B') result += 3;
+		if (tmp == 'N') result += 3;
+		if (tmp == 'P') result += 1;
+		if (tmp == 'q') result -= 9;
+		if (tmp == 'r') result -= 5;
+		if (tmp == 'b') result -= 3;
+		if (tmp == 'n') result -= 3;
+		if (tmp == 'p') result -= 1;
+	}
+	if (result > 0)cout << "White";
+	else if (result == 0)cout << "Draw";
+	else cout << "Black";
+	
+	//_getch();
+	return 0;
+}
