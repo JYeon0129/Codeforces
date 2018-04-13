0f1915a3fb706e2a469e482c49b2142a53b76219
JYeon0129 <wkddus119@naver.com>
2018-04-13 17:19:06 +0900

    Add files via upload
    집 A에서 집 B로 파이프가 나가고 들어가는것이 각각 최대 1개씩만 존재할 때
    한 집에서 이어진 마지막 집까지 운반할수 있는 물의 양과 시작과 끝을 출력

---
+++ 107A. Dorm Water Supply.cpp
@@ -0,0 +1,67 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+#include<algorithm>
+#include<functional> 
+#include<vector>
+#include<map>
+#include<conio.h>
+using namespace std;
+vector<vector<int>> v;
+int *arr;
+vector<string> vresult;
+bool isInclude(int a, int b[], int n) {
+	for (int i = 0; i < n; i++) {
+		if (a == b[i]) return false;
+	}
+	return true;
+}
+int* dfs(int index, int water) {
+	int d = water;
+	int *result;
+	result = new int[2];
+	result[0] = index;
+	result[1] = d;
+	if (v[index].size() > 0) {
+		if (v[index].at(1) < d) d = v[index].at(1);
+		v[index].at(2) = -1;
+		result = dfs(v[index].at(0), d);
+	}
+	return result;
+}
+int main() {
+	int n, p, a, b, d;
+	int *result;
+	result = new int[2];
+	cin >> n >> p;
+	v.resize(n + 1);
+	arr = new int[p];
+	for (int i = 0; i < p; i++) {
+		cin >> a >> b >> d;
+		v[a].push_back(b);
+		v[a].push_back(d);
+		v[a].push_back(0);
+		arr[i] = b;
+	}
+
+	if (n == p || p ==0) {
+		cout << "0";
+	}
+	else {
+		a = 0;
+		for (int i = 1; i < n + 1; i++) {
+			if (v[i].size() > 0 && v[i].at(2) == 0) {
+				if (isInclude(i, arr, p)) {
+					result = dfs(i, INT32_MAX);
+					a++;
+					vresult.push_back(to_string(i)+ " " + to_string(result[0]) + " " + to_string(result[1]));
+				}
+			}
+		}
+		cout << a << endl;
+		for (int i = 0; i < vresult.size(); i++)
+			cout << vresult.at(i) << endl;
+	}
+	//_getch();
+	return 0;
+}
