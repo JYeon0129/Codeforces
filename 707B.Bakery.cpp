33586fb97b534304b5ae514891eef0e1bd32ad84
JYeon0129 <wkddus119@naver.com>
2018-04-13 19:45:26 +0900

    Add files via upload
    창고가 아닌 도시중 가장 가까운 도시를 찾아라

---
+++ 707B.Bakery.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<string>
+#include<math.h>
+#include<algorithm>
+#include<functional> 
+#include<vector>
+#include<map>
+#include<conio.h>
+using namespace std;
+int graph[100001][3];
+int arr[100001];
+int main() {
+	int n, m, k, t1;
+	cin >> n >> m >> k;
+	for (int i = 0; i < m; i++) {
+		cin >> graph[i][0] >> graph[i][1] >> graph[i][2];
+	}
+	for (int i = 0; i < k; i++) {
+		cin >> t1;
+		arr[t1] = 1;
+	}
+	int s = -1;
+	for (int i = 0; i < m; i++) {
+		if (arr[graph[i][0]] + arr[graph[i][1]] == 1) {
+			if (s < 0) s = graph[i][2];
+			else if (s > 0 && graph[i][2] < s) s = graph[i][2];
+		}
+	}
+	cout << s;
+	//_getch();
+	return 0;
+}
